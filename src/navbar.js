860ed745022376baa945e0afc834b4b8d524ff32
Cristian <inesherald@gmail.com>
2019-12-31 19:17:59 -0600

    Add layout and input search

--- src/navbar.js
+++ src/navbar.js
@@ -1,8 +1,23 @@
 function showNavbar() {
   const element = document.createElement('div');
+  const inputSearch = document.createElement('input');
+  const toggletempContainer = document.createElement('div');
+  const radioC = document.createElement('input');
+  const radioF = document.createElement('input');
+
+  radioC.setAttribute('type', 'radio');
+  radioC.setAttribute('name', 'radioC');
+  radioF.setAttribute('type', 'radio');
+  radioF.setAttribute('name', 'radioF');
+  toggletempContainer.id = 'toggle-temp';
   element.id = 'navcontent';
+  inputSearch.id = 'search-box';
+  inputSearch.placeholder = 'Enter your city name';
   document.body.appendChild(element);
-  alert('this navbar');
+  element.appendChild(inputSearch);
+  element.appendChild(toggletempContainer);
+  toggletempContainer.appendChild(radioC);
+  toggletempContainer.appendChild(radioF);
   return element;
 }
 
