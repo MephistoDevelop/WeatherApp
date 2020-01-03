e60763027c169ac7317985155ef3ad3270e1601f
Cristian <inesherald@gmail.com>
2020-01-03 01:31:54 -0600

    Add querys to api

--- src/navbar.js
+++ src/navbar.js
@@ -4,11 +4,15 @@ function showNavbar() {
   const toggletempContainer = document.createElement('div');
   const radioC = document.createElement('input');
   const radioF = document.createElement('input');
+  const textF = document.createElement('span');
+  const textC = document.createElement('span');
 
   radioC.setAttribute('type', 'radio');
   radioC.setAttribute('name', 'radioC');
   radioF.setAttribute('type', 'radio');
   radioF.setAttribute('name', 'radioF');
+  textC.textContent = '°C';
+  textF.textContent = '°F';
   toggletempContainer.id = 'toggle-temp';
   element.id = 'navcontent';
   inputSearch.id = 'search-box';
@@ -16,13 +20,32 @@ function showNavbar() {
   document.body.appendChild(element);
   element.appendChild(inputSearch);
   element.appendChild(toggletempContainer);
+
   toggletempContainer.appendChild(radioC);
+  toggletempContainer.appendChild(textC);
   toggletempContainer.appendChild(radioF);
+  toggletempContainer.appendChild(textF);
   inputSearch.onkeypress = function(e) {
     if (e.keyCode === 13) {
-      let city = document.getElementById('search-box');
-      alert('Enter pressed: ' + city.value);
-      city.value = '';
+      let city = document.getElementById('search-box').value;
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=2a34c0d5ae373ea1442c4619767d8f09`,
+        { mode: 'cors' }
+      )
+        .then(function(response) {
+          // Successful response :)
+          let value = response.json();
+
+          value.then(function(result) {
+            console.log(result);
+          });
+        })
+        .catch(function(err) {
+          // Error :(
+          alert('Error in query , try again when your conections improve. ');
+        });
+
+      document.getElementById('search-box').value = '';
     }
   };
   return element;
