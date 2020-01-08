aa350f470b2b1c344c89213c243ca89ff7992044
Cristian <inesherald@gmail.com>
2020-01-08 07:28:14 -0600

    Add metatags

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import showNavbar from './navbar';
 import showContent from './contentContainer';
 (() => {
   const mainContainer = document.getElementById('content');
+  const footer = document.createElement('div');
+  footer.textContent = 'MephistoDevelop ® 2019';
+  footer.id = 'footer-text';
   mainContainer.appendChild(showNavbar());
   showContent();
+  mainContainer.appendChild(footer);
 })();
